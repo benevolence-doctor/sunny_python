f1fffb8b316c781fcaf8f3fa6faedadd3ec40ee9
benevolence-doctor <benevolence-doctor@users.noreply.github.com>
2015-07-30 17:43:50 +0800

    Update decodeurl.py

--- ipython/decodeurl.py
+++ ipython/decodeurl.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib;
-for deurl in open('url') : 
+import urllib
+import sys
+import os
+#for deurl in open('gjurl') : 
+for deurl in open(sys.argv[1]) :     ###传参数类似shell脚本中的$1 $2
     decodedUrl = urllib.unquote(deurl)
     print decodedUrl,
